f657c59c4d9119c1a317d77a1af092802f8002c8
Sushant Bailkar <sushantbailkar2504@gmail.com>
2023-09-21 12:45:11 +0530

    minor cahnges

--- src/Pages/Success.js
+++ src/Pages/Success.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { PulseLoader } from "react-spinners";
 
 const Success = () => {
+  const navigate = useNavigate();
+  const cartItems = useSelector((state) => state.cart.cartItems);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     setTimeout(() => {
@@ -14,10 +18,22 @@ const Success = () => {
         <PulseLoader color="#36d7b7" />
       ) : (
         <>
+        
           <h1 className="text-2xl font-semibold text-gray-700 mb-4">
             Order Successful!
           </h1>
-          <p>Your order has been successfully placed</p>
+          <p className="mb-6">Your order has been successfully placed</p>
+         
+        <div className="flex justify-center items-center flex-col bg-slate-400 p-2 rounded">
+        {cartItems.map((itm,idx)=><Card item={itm} key={idx}/>)}
+        </div>
+        
+          <button
+            className="bg-green-500 w-[90vw] lg:w-[12vw] rounded p-2 mt-6 hover:bg-green-700 hover:scale-110 transition-all duration-500"
+            onClick={() => navigate("/")}
+          >
+            Go Back
+          </button>
         </>
       )}
     </div>
@@ -25,3 +41,12 @@ const Success = () => {
 };
 
 export default Success;
+
+const Card = ({item}) => {
+  return (
+    <div className="flex w-[60vw] justify-end mt-4  items-center">
+      <h1 className="font-semibold text-gray-800 w-[60%]">{item.name} :</h1>
+      <p className="w-[25%] ">{item.qty}</p>
+    </div>
+  );
+};
